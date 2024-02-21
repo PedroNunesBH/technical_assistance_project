11a1619b769e580074dc2bed85c907f170171839
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-02-21 15:51:34 -0300

    Criacao do model Devices e registro no administrador

--- devices/admin.py
+++ devices/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Devices
 
-# Register your models here.
+
+class DevicesAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Devices._meta.fields]
+    search_fields = ("model", "name")
+
+
+admin.site.register(Devices, DevicesAdmin)
--- devices/models.py
+++ devices/models.py
@@ -1,1 +1,21 @@
+from django.db import models
 
+
+STATUS = [
+    ("Em Andamento", "Em Andamento"),
+    ("Aguardando Retirada", "Aguardando Retirada"),
+    ("Em Orçamento", "Em Orçamento"),
+]
+
+
+class Devices(models.Model):
+    model = models.CharField(max_length=100)
+    client_name = models.CharField(max_length=100)
+    client_phone = models.CharField(max_length=30)
+    client_email = models.EmailField()
+    entry_date = models.DateTimeField(
+        auto_now_add=True
+    )  # Adiciona automaticamente data e hora de entrada do aparelho
+    status = models.CharField(choices=STATUS, max_length=50)
+    technician = models.CharField(max_length=50)
+    problem_description = models.TextField()
