28240efab6ea25700a2019299c40699676a92bc3
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-02-21 17:40:45 -0300

    Criacao da view e url para listagem de todos os aparelhos

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from devices.views import HomePage
+from devices.views import HomePage, ListDevicesView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("homepage/", HomePage.as_view(), name='homepage')
+    path("homepage/", HomePage.as_view(), name='homepage'),
+    path("all_devices/", ListDevicesView.as_view(), name='list_devices')
 ]
--- devices/views.py
+++ devices/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Devices
 
 
 class HomePage(TemplateView):
     template_name = "base.html"
+
+
+class ListDevicesView(ListView):
+    template_name = "list_devices.html"
+    model = Devices
