bdb90fec3b9e2765a3b7d6a3452b6ac308390a6a
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-03-11 18:25:05 -0300

    Criacao de labels para os campos de formulario de 'CreateTechnicalsForm'

--- technicals/forms.py
+++ technicals/forms.py
@@ -7,3 +7,11 @@ class CreateTechnicalsForm(ModelForm):
     class Meta:
         model = Technicals
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(CreateTechnicalsForm, self).__init__(*args, **kwargs)
+        self.fields["name"].label = "Nome"
+        self.fields["cpf"].label = "CPF"
+        self.fields["admission_date"].label = "Data de Admissão"
+        self.fields["salary"].label = "Salário"
+        self.fields["phone_number"].label = "Número de Telefone"
