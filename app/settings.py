4e97a73609302ee1d8c14c82f3b69de18dab3c15
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-03-11 17:34:23 -0300

    Criacao do signal 'device_status_update' para comunicar o cliente sobre alterações no status de conserto

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from envinronment_variables import email, senha
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,10 @@ LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "list_devices"  # Pagina de redirecionamento após o login
 
 LOGOUT_REDIRECT_URL = "login"  # Pagina de redirecionamento após o logout
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # servidor do email a ser utilizado
+EMAIL_PORT = 587  # porta do servidor
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = email  # endereco de email do remetente
+EMAIL_HOST_PASSWORD = senha  # senha do endereco de email do remetente
---
+++ devices/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import Devices
+
+
+@receiver(pre_save, sender=Devices)
+def device_status_update(sender, instance, **kwargs):
+    if instance.pk is not None:  # Verifica se não é a criação de um objeto
+        device = Devices.objects.get(pk=instance.pk)  # Captura o objeto
+        if device.status != instance.status:
+            subject = f'Atualização do Status do Seu Produto {instance.model}'  # Assunto do email
+            message = (f'Olá {instance.client_name}.\n'
+                       f'Estamos passando para informar que o status do seu produto {instance.model}'
+                       f' foi atualizado para {instance.status}.\n'
+                       f'Qualquer coisa fique a vontade para entrar em contato com nossa equipe.')
+            from_email = settings.DEFAULT_FROM_EMAIL
+            to_email = [f'{instance.client_email}']  # Email do destinatário
+
+            send_mail(subject, message, from_email, to_email)
