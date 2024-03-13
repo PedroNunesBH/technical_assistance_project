657b5f3fbd9260b99c40bb63229e50fec753bb36
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-03-13 17:30:13 -0300

    Alterações no template da página de login(LoginView)

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", LoginView.as_view(), name="login"),
+    path("", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("", include("devices.urls")),
     path("", include("technicals.urls")),
